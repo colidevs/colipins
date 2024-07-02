9210886dcf2704fc23284b96fc6ef50c023d8fb0
nicolasvega4 <nicolas.ha.vega@gmail.com>
2024-07-02 20:50:51 -0300

    Añadi lista de imagenes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
-import {Input} from "@/components/ui/input";
+import Image from "next/image";
 
-export default function HomePage() {
-  return <Input placeholder="Search" type="search" />;
+import {Input} from "@/components/ui/input";
+import {api} from "@/api";
+
+export default async function HomePage() {
+  // CODIGO, funcionalidades, logica
+  const photos = await api.photos.list();
+
+  // RETURN => JSX, programacion declarativa, estructura de la pagina, vista, elementos visuales
+  return (
+    <>
+      <h1>Home Page</h1>
+      <Input placeholder="Search" type="search" />
+
+      <ul>
+        {photos.map((photo) => (
+          <li key={photo.id}>
+            <Image
+              alt={photo.alt_description!}
+              height={photo.height}
+              src={photo.urls.raw}
+              width={photo.width}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
