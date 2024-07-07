d61a3a717b54b22075ce5ad395bbe08df498b33a
nicolasvega4 <nicolas.ha.vega@gmail.com>
2024-07-07 20:53:20 -0300

    Agregue comentarios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,18 @@ export default async function HomePage() {
   const photos = await api.photos.list();
 
   // RETURN => JSX, programacion declarativa, estructura de la pagina, vista, elementos visuales
+  // Muestra de cambios
+
   return (
     <>
       <h1>Home Page</h1>
       <Input placeholder="Search" type="search" />
-
       <ul>
         {photos.map((photo) => (
           <li key={photo.id}>
             <Image
               alt={photo.alt_description!}
+              className="flex"
               height={photo.height}
               src={photo.urls.raw}
               width={photo.width}
