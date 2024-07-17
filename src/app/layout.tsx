b35f3c47cec0766d66eef389f90f64ded5ad30e4
pufi <thomas.r.pufahl@gmail.com>
2024-07-16 23:46:41 -0300

    fix: no title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
     <html lang="en">
       <body className="container m-auto grid min-h-screen grid-rows-[auto,1fr,auto] bg-background px-4 font-sans antialiased">
         <header className="text-xl font-bold leading-[4rem]">
-          <Link href="/"> 🖼️</Link>
+          <Link href="/">Colipins 🖼️</Link>
         </header>
         <main className="py-8">{children}</main>
         <footer className="text-center leading-[4rem] opacity-70">
